46493b7c110f666ed7f68e16017f9e9be343a3af
hugo benichi <hugo.benichi@m4x.org>
2012-07-24 13:39:49 +0900

    prob40 cleaned

--- prob40.rb
+++ prob40.rb
@@ -1,7 +1,8 @@
 #!/usr/local/bin/ruby
 
-lim = 200000 #this generates a big enough string
+"".tap{ |decimals|
+  200000.times{ |i| decimals << i.to_s } # 200000 generates a big enough string
+  puts [1e0,1e1,1e2,1e3,1e4,1e5,1e6].map{|i| decimals[i].to_i }.inject(1){|p,x| p *= x }
+}
 
-all = (1..lim).each.inject("0"){ |s,x| s << x.to_s }
 
-puts [1e0,1e1,1e2,1e3,1e4,1e5,1e6].map{ |i| all[i.to_i].to_i }.inject(1){|p,x|p *= x}
