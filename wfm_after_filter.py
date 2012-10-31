a4c3f41c08c81959a16635a1a594a2cd8be02d5a
hugo benichi <hugo.benichi@m4x.org>
2012-10-31 11:40:39 +0900

    added option for setting cutoff in wfm_after_filter.py.

--- wfm_after_filter.py
+++ wfm_after_filter.py
@@ -14,26 +14,27 @@
 
 help = """
   this script attempts to read a binary file of waveforms
-  encoded in signed char and computes its mean fft spectrum
+  encoded in signed char, filters it and then compute the
+  average waveform and the stacked trace image of all
+  filtered waveform
   
   argument: 
     path/to/input/binary/file    (1st place on the command line)
 
   options:
     -h,        --help            print this help
+    -c=BAND,   --cutoff=BAND     set the passband upper frequency (in Hz)
     -l=LEN,    --length=LEN      set the number of data point in one waveform
     -f=FRAME,  --frame=FRAME     set the number of total waveforms to read
     -r=RATE,   --rate=RATE       set the sampling interval (in sec)
     -s=OUTPUT, --save=OUTPUT     set the path for saving output data (default: no ouput)
-    -p,        --plot            show the fft spectrum
+    -p,        --plot            show the different graphs
 
   comments:
-    - you need to have the "tes" python package on your python path ()
+    - you need to have the "tes" python package on your python path
     - if neither -s nor -p options are specified, nothing happens
-    - spectrum data are saved in textual format using numpy.savetxt.
-        1st line: frequency axis, space seperated
-        2nd line: frequency components in dB, space seperated
-        use numpy.loadtxt("path/to/data") to reload the data
+    - data are saved in textual format using numpy.savetxt. check
+      the end of the script for details
 
   additional info @ github.com/hugobenichi/tes
 """
@@ -56,8 +57,10 @@ frame  = -1
 length = 1000
 to_plot= False
 rate   = 5 * 1e-9
+cutoff = 10 * 1e6  # 10 MHz
 
-opt_short, opt_long = "r:f:l:s:ph", [ "rate=", "frame=", "length=", "save=", "plot", "help"]
+opt_short = "c:r:f:l:s:ph"
+opt_long  = [ "cutoff=", "rate=", "frame=", "length=", "save=", "plot", "help"]
 options, args = getopt.getopt( sys.argv[2:], opt_short, opt_long )
 
 try:
@@ -70,6 +73,7 @@ try:
 		if option in ('-f', '--frame'):     frame   = int(value)
 		if option in ('-s', '--save'):      output  = value
 		if option in ('-r', '--rate'):      rate    = float(value)
+		if option in ('-c', '--cutoff'):    cutoff  = float(value)
 except TypeError:
 	sys.stderr.write("bad argument")
 	sys.exit(22)
@@ -82,10 +86,8 @@ freq = tes.waveform.freq(rate, length)
 time = tes.waveform.time(rate, length)
 
 
-cutoff = 50 * 1e6           # 50 MHz  -> 10 MHz -> 5 MHz
 notch_start = 2.6 * 1e6
 notch_stop  = 5.0 * 1e6
-
 band_index, notch_i_s, notch_i_e = 0, 0, 0
 
 while freq[band_index] < cutoff: band_index += 1
@@ -98,13 +100,13 @@ for waveform in tes.waveform.read_binary( path=input, length = length, frame = f
 	wfmtrace.add(filtered)
 
 mean_wfm_data = [ time, mean_wfm.compute() ]
-wfmtrace_data = [ time, wfmtrace.compute() ]
+wfmtrace_data = [ time, wfmtrace.compute() ] # cannot be passed directly to numpy.savetxt
 
 tes.plot.waveform( mean_wfm_data, output, show = to_plot )
 tes.plot.trace( wfmtrace_data, output, show = to_plot )
 
 if output is not None: 
 	numpy.savetxt( output + "meanwfm" + ".val", mean_wfm_data )
-	numpy.savetxt( output + "trace"    + ".val", wfmtrace_data )
+	numpy.savetxt( output + "trace"    + ".val", wfmtrace_data[1] )
 
 
