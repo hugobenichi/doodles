b16ef75aaae35bc2f3213265aab23e800e26393e
hugo benichi <hugo.benichi@m4x.org>
2012-11-06 17:39:22 +0900

    just testing

--- pulse_model_test.py
+++ pulse_model_test.py
@@ -23,7 +23,7 @@ import tes.histogram
 input  = sys.argv[1]
 output = sys.argv[3]
 length = 1000
-frame  = -1 #2000
+frame  = 2000
 rate   = 5 * 1e-9
 dc     = -87
 
@@ -59,7 +59,6 @@ cutoff = 20 * 1e6
 band_index = tes.waveform.freq_index(freq,cutoff)
 
 hist_raw = tes.histogram.byte1()
-hist_max = tes.histogram.byte1()
 hist_fit = tes.histogram.byte1()
 hist_fitw = tes.histogram.byte1()
 hist_fitd = tes.histogram.byte1()
@@ -81,20 +80,17 @@ for waveform in tes.waveform.read_binary( path=input, length=length, frame=frame
 	tes.histogram.add( hist_fit, dc+e1 )
 	tes.histogram.add( hist_fitw, dc+e2 )
 	tes.histogram.add( hist_fitd, dc+e3 )
-	tes.histogram.add( hist_max, dc+max(dc_wfm) )
 
 volt = numpy.arange(0,256)
-#tes.plot.waveform( [volt, hist_raw, hist_fit, hist_fitw ], save=output )
-tes.plot.waveform( [volt,hist_raw, hist_fit], xlim=(50,250), save=output + "_fit_")
-tes.plot.waveform( [volt,hist_raw,hist_fitw], xlim=(50,250), save=output + "_fitw_")
-tes.plot.waveform( [volt,hist_raw,hist_fitd], xlim=(50,250), save=output + "_fitd_")
-tes.plot.waveform( [volt,hist_raw,hist_max], xlim=(50,250), save=output + "_max_")
+tes.plot.waveform( [volt, hist_raw, hist_fit, hist_fitw ])#, save=output )
+tes.plot.waveform( [volt,hist_raw, hist_fit], xlim=(50,250))#, save=output + "_fit_")
+tes.plot.waveform( [volt,hist_raw,hist_fitw], xlim=(50,250))#, save=output + "_fitw_")
+tes.plot.waveform( [volt,hist_raw,hist_fitd], xlim=(50,250))#, save=output + "_fitd_")
 
 print( "raw:                   ", tes.histogram.visibility(hist_raw, 8) )
 print( "fit:                   ", tes.histogram.visibility(hist_fit, 8) )
 print( "weighted fit:          ", tes.histogram.visibility(hist_fitw, 8) )
 print( "derivate weighted fit: ", tes.histogram.visibility(hist_fitd, 8) )
-print( "max:                   ", tes.histogram.visibility(hist_max, 8) )
 
 
 #numpy.savetxt( sys.argv[2] + "_raw.val", hist_raw )
