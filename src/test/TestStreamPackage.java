43d8c018737c4d35e2de13980d4fd9d2c23b156e
hugo benichi <hugo.benichi@m4x.org>
2013-02-18 16:22:19 +0900

    initial helloworld test file

---
+++ src/test/TestStreamPackage.java
@@ -0,0 +1,13 @@
+import streams.Streams;
+
+public class TestStreamPackage {
+
+    private TestStreamPackage() {}
+
+    public static void main(String argv[]) {
+
+        Streams.foo();
+        System.exit(0);
+
+    }
+}
