38c75e62089c057b1234b37bfc5ec4b03d02dcb6
hugo benichi <hugo.benichi@m4x.org>
2013-02-18 18:56:56 +0900

    adding version number changing script

---
+++ change_version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version=`cat VERSION`
+
+echo "changing version number to "$version" in ..."
+
+
+for source_file in src/streams/*.java
+do
+    echo $source_file
+    sed "s|[0-9]\.[0-9]\.[0.9]|$version|" $source_file > $source_file
+done
+
