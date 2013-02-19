bdb3b4e8709371a5998caadc3507500efbc3dc3f
hugo benichi <hugo.benichi@m4x.org>
2013-02-19 15:36:41 +0900

    finished implementing AbstractStream

--- src/streams/AbstractStream.java
+++ src/streams/AbstractStream.java
@@ -49,7 +49,46 @@ public abstract class AbstractStream<E> implements Stream<E> {
      * @see Predicate
      */
     public Stream<E> select(Predicate<? super E> check) {
-        return this;
+
+        final Iterable<E> input_stream = this;
+        return new AbstractStream<E>() {
+
+            public Iterator<E> iterator() {
+                return new Iterator<E>(){
+
+                    Iterator<E> iter = input_stream.iterator();
+                    E next_item = null;
+                    boolean is_next_valid = false;
+
+                    private boolean find_next_valid() {
+                        while( is_next_valid == false && iter.hasNext() ) {
+                            E next_candidate = iter.next();
+                            if ( check.call(next_candidate) ) {
+                                is_next_valid = true;
+                                next_item = next_candidate;
+                            }
+                        }
+                        return is_next_valid;
+                    }
+
+                    public void remove(){}
+                    public boolean hasNext() {
+                        if (is_next_valid == false)
+                            is_next_valid = find_next_valid();
+                        return is_next_valid;
+                    }
+
+                    public E next() { 
+                        if (is_next_valid == false)
+                            is_next_valid = find_next_valid();
+                        return is_next_valid ? next_item : null;
+                    }
+
+                };
+            }
+
+        };
+
     }
 
     // TODO: shouldn t first param of Operator be ? extend E ?
@@ -59,12 +98,23 @@ public abstract class AbstractStream<E> implements Stream<E> {
      * @return        the final state of the reduce operation.
      * @see Operator
      */
-    public E reduce(Operator<? extends E, ? super E> reducer) {
-        /*
-            get init_state from iterator
-            call fold to leverage
-        */
-        return null;
+    public E reduce(Operator<E, ? super E> reducer) {
+
+        Function<E,E> reducing_adapter = new Function<E,E>() {
+            boolean is_init = false;
+            E accumulator_state = null;
+            public E call(E input){
+                if (is_init) {
+                    accumulator_state = reducer.call(accumulator_state, input);
+                } else {
+                    is_init = true;
+                    accumulator_state = input;
+                }
+                return accumulator_state;
+            }
+        };
+
+        return fold_from_map(reducing_adapter);
     }
 
     // TODO: check food type of init_state
@@ -77,13 +127,24 @@ public abstract class AbstractStream<E> implements Stream<E> {
      * @return           the final state of the fold operation.
      * @see Operator
      */
-    public <F> F fold(Operator<? extends F, ? super E> folder, F init_state) {
-        return null;
-        /*
-            prepare map operator which apply folder and return init_state
-            define stream with this map operator
-            return last state with last
-        */
+    public <F> F fold(Operator<F, ? super E> folder, F init_state) {
+
+        Function<E,F> folding_adapter = new Function<E,F>() {
+            F accumulator_state = init_state;
+            public F call(E input){
+                accumulator_state = folder.call(accumulator_state, input);
+                return accumulator_state;
+            }
+        };
+
+        return fold_from_map(folding_adapter);
+    }
+
+    // helper function for fold and reduce
+    // TODO: DOCME!
+    private <F> F fold_from_map(Function<E,F> folding_adapter) {
+        Stream<F> folding_stream = this.map(folding_adapter);
+        return Streams.last(folding_stream);
     }
 
 }
