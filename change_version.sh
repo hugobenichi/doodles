90d74b2cecab3780a7e8c6632700174a0c6c968c
hugo benichi <hugo.benichi@m4x.org>
2013-02-20 12:56:25 +0900

    bug in change_version script

--- change_version.sh
+++ change_version.sh
@@ -8,6 +8,6 @@ echo "changing version number to "$version" in ..."
 for source_file in src/streams/*.java
 do
     echo $source_file
-    sed "s|[0-9]\.[0-9]\.[0.9]|$version|" $source_file > $source_file
+    sed "s|[0-9]\.[0-9]\.[0-9]|$version|" $source_file > $source_file
 done
 
