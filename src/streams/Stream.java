93b8d50d3861a8ef977832b64d253076d20b3b87
hb <hugo@Ulgrim.(none)>
2013-02-22 00:46:25 +0900

    typo in doc

--- src/streams/Stream.java
+++ src/streams/Stream.java
@@ -9,7 +9,7 @@ import streams.Predicate;
 
 /**
  * Stream defines a wrapper for the Iterable interface and adds four useful
- * functions to apply and chain operations on iterators. All operations shloud
+ * functions to apply and chain operations on iterators. All operations should
  * be conducted in a lazy fashion: values are computed only when necessary until
  * some outside iteration needs them. The preferred way to build Stream objects
  * is via the static factory method Streams#from.
