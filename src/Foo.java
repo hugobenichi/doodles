896738f986ba562a44f3221a9a8b1b9a41d66d8f
Hugo Benichi <hugo@midokura.com>
2014-01-03 10:49:15 +0900

    code snippet with lambda and stream

---
+++ src/Foo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.function.*;
+
+ class Foo {
+
+  public static void main(String[] args) {
+
+    List<String> l = Arrays.asList("foo","bar","etron","tokyo","paris");
+
+    l.forEach( s -> System.out.println(s) );
+
+    l.stream().map( s -> s.length() ).filter( len -> len > 3 ).forEach( len -> System.out.println(len) );
+
+    System.out.println("hello java 8");
+    System.exit(0);
+  }
+
+}
