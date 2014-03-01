a9711dcd096d9bb706011ef7e7a7bdf091f67aa7
Hugo Benichi <hugo@midokura.com>
2014-03-01 13:39:25 +0900

    running go fmt

--- worktime/dist.go
+++ worktime/dist.go
@@ -8,7 +8,7 @@ import (
 /* returns a gaussian distribution sampler as a callable function */
 func gaussian_sampler(sigma float64) func() int {
 	return func() int {
-    return int(sigma * rand.NormFloat64())
+		return int(sigma * rand.NormFloat64())
 	}
 }
 
@@ -16,11 +16,11 @@ func gaussian_sampler(sigma float64) func() int {
 func poisson_sampler(mean float64) func() int {
 	return func() int {
 		n := 0
-		prob := math.Exp(-mean)             // p(n==0)
-		cumul := rand.Float64() - prob      // start in [0.0, 1.0[
+		prob := math.Exp(-mean)        // p(n==0)
+		cumul := rand.Float64() - prob // start in [0.0, 1.0[
 		for cumul > 0.0 && n != 100 {
-			n++                               // order of statement matters
-			prob = prob * mean / float64(n)   // Poiss prob of next n value
+			n++                             // order of statement matters
+			prob = prob * mean / float64(n) // Poiss prob of next n value
 			cumul = cumul - prob
 		}
 		return n
--- worktime/worktime.go
+++ worktime/worktime.go
@@ -1,34 +1,36 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "strconv"
+	"fmt"
+	"net/http"
+	"strconv"
 )
 
 func get_days(r *http.Request) int {
 	days, err := strconv.ParseInt(r.Form.Get("days"), 10, 8)
-  if err != nil || days < 0 { days = 20 }
-  return int(days)
+	if err != nil || days < 0 {
+		days = 20
+	}
+	return int(days)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  r.ParseForm()
+	r.ParseForm()
 
-  days := get_days(r)
+	days := get_days(r)
 	from := Time{9, 30}
 	to := Time{18, 30}
 
-  unit := 15
+	unit := 15
 	sigma := 3.0
 
-  cfg := SimConf{days, from, to, sigma, unit}
+	cfg := SimConf{days, from, to, sigma, unit}
 
-  info, wdays := cfg.Simulate()
+	info, wdays := cfg.Simulate()
 
-  for _, s := range info {
+	for _, s := range info {
 		fmt.Fprintln(w, s)
-  }
+	}
 
 	for _, d := range wdays {
 		fmt.Fprintln(w, d)
@@ -37,5 +39,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func init() {
-  http.HandleFunc("/sim", handler)
+	http.HandleFunc("/sim", handler)
 }
