d6b6ce03c171dc5bc6ec61dbe7a3a876cf0ca519
Hugo Benichi <hugo@Oswald.local>
2014-09-03 07:38:59 +0900

    more js trials

--- js/discovery/program.js
+++ js/discovery/program.js
@@ -1,1 +1,27 @@
 document.writeln("hello world")
+
+// testing function scopes
+var foo = function () {
+  var a = 5;
+  var b = 7;
+  var bar = function () {
+    var b = 10;
+    var c = 32;
+    a = b + c;
+  }
+  document.writeln([a, b]);
+  var d = bar()
+  document.writeln([a, b]);
+};
+
+foo();
+
+document.writeln([] + {})
+
+document.writeln({} + [])
+
+document.writeln({} + {})
+
+document.writeln({} - {})
+
+document.writeln(Array(16).join("wat" - 1) + ' Batman !')
