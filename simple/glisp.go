a33db001768dbb92ed4c3b444fbe270105d34fb5
hugo benichi <hbenichi@gmail.com>
2014-10-04 21:24:36 +0900

    glisp: basically complete, except for recursion
    - added a environment encoded with List
    - added closures

--- simple/glisp.go
+++ simple/glisp.go
@@ -3,10 +3,6 @@
 //  A basic lisp interpreter in go
 //
 //  left to implement
-//    - define intrinsic
-//    - do some basic testing
-//    - turn ListEnv into regular List for closures
-//    - implement closures in Fn
 //    - try the Ycomb
 //    - add defn to define recursive functions
 //
@@ -17,63 +13,44 @@ import (
   "errors"
 )
 
+
+//  -- Basic Values ------------------------------------------------------------
+
+
 // tag interface to define the ensemble of possible values
 type Val interface {
   implement()
   String() string
 }
 
-//  -- Basic Values ------------------------------------------------------------
-//  are also expressions.
 
 type Atom string
+func (a Atom) implement() { }
+func (a Atom) String() string { return fmt.Sprintf("Atom(%s)", string(a)) }
 
 type Num int
+func (n Num) implement() { }
+func (n Num) String() string { return fmt.Sprintf("%d", int(n)) }
 
 type List struct {
   Head Val
   Tail *List
 }
-
-type Intrinsic func(*List) Val
-
-func (a Atom) implement() { }
-func (a Atom) String() string { return fmt.Sprintf("Atom(%s)", string(a)) }
-
-func (n Num) implement() { }
-func (n Num) String() string { return fmt.Sprintf("%d", int(n)) }
-
 func (l *List) implement() { }
 func (l *List) String() string { return fmt.Sprintf("%#v", l) }
+func (l *List) Cons(v Val) *List { return &List{ v, l } }
 
-func (l *List) Cons(v Val) *List {
-  return &List{ v, l }
-}
-
+type Intrinsic func(environment Val, args *List) Val
 func (i Intrinsic) implement() { }
 func (i Intrinsic) String() string { return "Intrinsic" }
 
 
-//  -- Environment
+//  -- Environment getter and setter -------------------------------------------
 
-type Env interface {
-  Get(a Atom) Val
-  Push(a Atom, v Val) Env
-}
-
-
-//  -- ListEnvironment
-
-// TODO: I need to change this into a regular List
-
-type ListEnv struct {
-  Atoms *List
-  Values *List
-}
-
-func (le ListEnv) Get(a Atom) Val {
-  atoms := le.Atoms
-  values := le.Values
+func Get(env Val, a Atom) Val {
+  ls := env.(*List)
+  atoms := ls.Head.(*List)
+  values := ls.Tail.Head.(*List)
   for atoms != nil && values != nil {
     if atoms.Head.(Atom) == a {
       return values.Head
@@ -84,17 +61,17 @@ func (le ListEnv) Get(a Atom) Val {
   panic(errors.New(fmt.Sprintf("Unbound variable '%s'", string(a))))
 }
 
-func (le ListEnv) Push(a Atom, v Val) Env {
-  return ListEnv{ le.Atoms.Cons(a), le.Values.Cons(v) }
+func Push(env Val, a Atom, v Val) Val {
+  ls := env.(*List)
+  atoms := ls.Head.(*List)
+  values := ls.Tail.Head.(*List)
+  return list(atoms.Cons(a), values.Cons(v))
 }
 
-func EmptyEnv() Env {
-  return ListEnv { }
-}
 
-// -- Evaluator
+// -- Evaluator ----------------------------------------------------------------
 
-func Eval(e Env, v Val) Val {
+func Eval(e Val, v Val) Val {
   switch t := v.(type) {
 
   default:
@@ -106,7 +83,7 @@ func Eval(e Env, v Val) Val {
 
   // An Atom evaluates to its bound values in the environement.
   case Atom:
-    return e.Get(t)
+    return Get(e, t)
 
   // A List node evaluates to a a function call
   case *List:
@@ -119,20 +96,18 @@ func Eval(e Env, v Val) Val {
 
     // if fn is an Intrasic, call it
     if intrinsic, ok := fn.(Intrinsic); ok {
-      return intrinsic(t.Tail)
+      return intrinsic(e, t.Tail)
     }
 
     // otherwise the head should eval to a function shape
     fnshape := fn.(*List)
-    // needs to turn ListEnv into a Val if I want to represent functions as vals
-    _ = fnshape.Head //.(ListEnv)
-    cloz := EmptyEnv() // for the time being, no closures
-    body := fnshape.Tail.Head
-    vars := fnshape.Tail.Tail
+    cloz := fnshape.Head
+    vars := fnshape.Tail.Head.(*List)
+    body := fnshape.Tail.Tail.Head
 
     // eval the tail expressions and bind them to the names in var
     for vars != nil {
-      cloz = cloz.Push(vars.Head.(Atom), Eval(e, vals.Head))
+      cloz = Push(cloz, vars.Head.(Atom), Eval(e, vals.Head))
       vars = vars.Tail
       vals = vals.Tail
     }
@@ -142,32 +117,130 @@ func Eval(e Env, v Val) Val {
 }
 
 
-// -- Intrinsic functions
+// -- Intrinsic functions ------------------------------------------------------
 
-var Fn = Intrinsic(func(l *List) Val { return l.Head })
+// lists
 
-var Head = Intrinsic(func(l *List) Val { return l.Head })
-
-var Tail = Intrinsic(func(l *List) Val { return l.Tail })
+var Head = Intrinsic(func(e Val, l *List) Val { return l.Head })
+var Tail = Intrinsic(func(e Val, l *List) Val { return l.Tail })
+var Ls = Intrinsic(func(e Val, l *List) Val { return l })
 
 // exactly the same ??
-var Cons = Intrinsic(func(l *List) Val { return l.Tail.Cons(l.Head) })
+var Cons = Intrinsic(func(e Val, l *List) Val { return l.Tail.Cons(l.Head) })
 
-var Ls = Intrinsic(func(l *List) Val { return l })
+// control
 
-var LetIn = 0
-var If = 0
-var Plus = 0
-var Mult = 0
+// BUG: empty list does not trigger false branch
+func If(e Val, l *List) Val {
+  conditional := Eval(e, l.Head)
+  if conditional == Num(0) || conditional == nil {
+    return Eval(e, l.Tail.Tail.Head)
+  }
+  return Eval(e, l.Tail.Head)
+}
+
+func Let(e Val, l *List) Val {
+  binding := l.Head.(*List)
+  body := l.Tail.Head
+  e = Push(e, binding.Head.(Atom), Eval(e, binding.Tail.Head))
+  return Eval(e, body)
+}
+
+func Fn(e Val, l *List) Val {
+  return l.Cons(e) // simply bind the current env to create the closure
+}
+
+// arithmetics
+
+func Plus(e Val, l *List) Val {
+  sum := 0
+  for l != nil {
+    v := Eval(e, l.Head)
+    if x, ok := v.(Num); ok {
+      sum += int(x)
+    }
+    l = l.Tail
+  }
+  return Num(sum)
+}
+
+func Mult(e Val, l *List) Val {
+  prod := 1
+  for l != nil {
+    v := Eval(e, l.Head)
+    if x, ok := v.(Num); ok {
+      prod = prod * int(x)
+    }
+    l = l.Tail
+  }
+  return Num(prod)
+}
+
+var plus = Atom("+")
+var mult = Atom("*")
+var cond = Atom("if")
+var let  = Atom("let")
+var null = Atom("nil")
+var fn   = Atom("fn")
+
+type Binding struct {
+  a Atom
+  v Val
+}
+
+var base_env Val = nil
+
+func NewEnv() Val {
+  if base_env == nil {
+    base_env = list(list(), list())
+    base_env = Push(base_env, plus, Intrinsic(Plus))
+    base_env = Push(base_env, mult, Intrinsic(Mult))
+    base_env = Push(base_env, cond, Intrinsic(If))
+    base_env = Push(base_env, let,  Intrinsic(Let))
+    base_env = Push(base_env, fn,   Intrinsic(Fn))
+    base_env = Push(base_env, null, list())
+  }
+  return base_env
+}
 
 
-// head, tail, cons
-// fn
-// if then else
-// let, letrec
+// -- Main program -------------------------------------------------------------
 
-// -- Main program
+
+func list(vals ...Val) Val {
+  var l *List = nil
+  for i := len(vals) - 1; i >= 0; i-- {
+    l = l.Cons(vals[i])
+  }
+  return l
+}
 
 func main() {
-  fmt.Println("")
+
+  x, y := Atom("x"), Atom("y")
+
+  p1 := list(plus, Num(3), Num(10))
+  fmt.Println(Eval(NewEnv(), p1))
+
+  p2 := list(mult, Num(1), Num(2))
+  fmt.Println(Eval(NewEnv(), p2))
+
+  p3 := list(cond, null, Num(2), Num(3))
+  fmt.Println(Eval(NewEnv(), p3))
+
+  p4 := list(let, list(x, Num(2)), list(plus, x, Num(3)))
+  fmt.Println(Eval(NewEnv(), p4))
+
+  p5 := list(list(fn, list(x), list(mult, x, Num(8))), Num(3))
+  fmt.Println(Eval(NewEnv(), p5))
+
+  p6 := list(
+          list(
+            list(fn, list(x),
+              list(fn, list(y),
+                list(mult, x, y))),
+            Num(5)),
+          Num(4))
+  fmt.Println(Eval(NewEnv(), p6))
+
 }
