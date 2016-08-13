4f71e9886a019240bcd85e2691eab954e1d3d962
Hugo Benichi <hugobenichi@gmail.com>
2016-08-13 21:14:12 +0900

    Fotoq: introduce Config struct for importing items

--- fotoq/fotoq.go
+++ fotoq/fotoq.go
@@ -12,62 +12,81 @@ import (
 /*
 	status:
 		list, scan, copy works
-		issue with modtime lost when copying files
 	next steps
-		define a Config struct for propagating constants
-		create test populating function
+		introduce Cmd enum for list, scan, copy
+		add timezone shift in Config
+		create test populating function,
+			how to exercice different modtime ?
+				use a parametrized function inside Config
 		write integration tests
 */
 
-// TODO: introduce config struct
+func main() {
+	//c := prod_cfg
+	c := test_cfg
+
+	//c.cmd_list()
+	c.cmd_scan()
+	//c.cmd_copy()
+}
+
+type Config struct {
+	source_root  string
+	staging_root string
+	exts         []string
+	dir_of_ext   map[string]string
+}
+
 var (
-	// TEST
-	source_root  = "/tmp/fotoq/test/in"
-	staging_root = "/tmp/fotoq/test/out"
-
-	// PROD
-	//source_root  = "/Volumes/Untitled/DCIM/"
-	//staging_root = "/Users/hugobenichi/Desktop/photos/staging"
-
-	exts = []string{
-		"JPG",
-		"RAF",
+	test_cfg = Config{
+		source_root:  "/tmp/fotoq/test/in",
+		staging_root: "/tmp/fotoq/test/out",
+		exts: []string{
+			"JPG",
+			"RAF",
+		},
+		dir_of_ext: map[string]string{
+			"RAF": "/raw",
+		},
 	}
-	dir_of_ext = map[string]string{
-		"RAF": "/raw",
+
+	prod_cfg = Config{
+		source_root:  "/Volumes/Untitled/DCIM/",
+		staging_root: "/Users/hugobenichi/Desktop/photos/staging",
+		exts: []string{
+			"JPG",
+			"RAF",
+		},
+		dir_of_ext: map[string]string{
+			"RAF": "/raw",
+		},
 	}
 )
 
-func main() {
-	//cmd_list()
-	//cmd_scan()
-	cmd_copy()
-}
-
-func cmd_list() {
-	for _, inf := range import_list(source_root, exts) {
+func (c *Config) cmd_list() {
+	for _, inf := range c.import_list() {
 		fmt.Printf("%s -> %s\n", inf.item, inf.staging)
 	}
 }
 
-func cmd_scan() {
-	by_staging := group_by_staging(import_list(source_root, exts))
+func (c *Config) cmd_scan() {
+	by_staging := group_by_staging(c.import_list())
 	for _, k := range sorted_keys_of(by_staging) {
 		fmt.Println(k)
 		for _, inf := range by_staging[k] {
-			fmt.Printf("  %s -> %s\n", inf.item, inf.new_path())
+			fmt.Printf("  %s -> %s\n", inf.item, inf.new_path_of())
 		}
 	}
 }
 
-func cmd_copy() {
-	by_staging := group_by_staging(import_list(source_root, exts))
+func (c *Config) cmd_copy() {
+	by_staging := group_by_staging(c.import_list())
 	for _, k := range sorted_keys_of(by_staging) {
 		fmt.Println(k)
 		die_if(os.MkdirAll(k, os.ModePerm))
 		for _, inf := range by_staging[k] {
-			fmt.Printf("  %s -> %s\n", inf.item, inf.new_path())
-			die_if(os.Rename(inf.item, inf.new_path()))
+			fmt.Printf("  %s -> %s\n", inf.item, inf.new_path_of())
+			die_if(os.Rename(inf.item, inf.new_path_of()))
 		}
 	}
 }
@@ -79,51 +98,51 @@ type ItemInfo struct {
 	modtime time.Time
 }
 
-func info_of(item string) ItemInfo {
+func (c *Config) info_of(item string) ItemInfo {
 	info, err := os.Stat(item)
 	die_if(err)
 	t := info.ModTime() // TODO: try to use creation time ?
-	e := suffix_of(item, exts)
+	e := c.suffix_of(item)
 	return ItemInfo{
 		item:    item,
 		ext:     e,
-		staging: staging_dir_of(staging_root, t, e),
+		staging: c.staging_dir_of(t, e),
 		modtime: t,
 	}
 }
 
-func (inf *ItemInfo) new_path() string {
+func (inf *ItemInfo) new_path_of() string {
 	return inf.staging + "/" + filepath.Base(inf.item)
 }
 
-func import_list(source_root string, exts []string) []ItemInfo {
+func (c *Config) import_list() []ItemInfo {
 	items := []ItemInfo{}
-	for _, e := range exts {
-		ms, err := filepath.Glob(pattern_of(source_root, e))
+	for _, e := range c.exts {
+		ms, err := filepath.Glob(c.pattern_of(e))
 		die_if(err)
 		for _, m := range ms {
-			items = append(items, info_of(m))
+			items = append(items, c.info_of(m))
 		}
 	}
 	return items
 }
 
-func pattern_of(root string, ext string) string {
-	return root + "/*/*." + ext
+func (c *Config) pattern_of(ext string) string {
+	return c.source_root + "/*/*." + ext
 }
 
-func suffix_of(item string, exts []string) string {
-	for _, e := range exts {
+func (c *Config) suffix_of(item string) string {
+	for _, e := range c.exts {
 		if strings.HasSuffix(item, e) {
 			return e
 		}
 	}
-	panic(fmt.Errorf("%s has no suffix in %v", item, exts))
+	panic(fmt.Errorf("%s has no suffix in %v", item, c.exts))
 }
 
-func staging_dir_of(staging_root string, t time.Time, ext string) string {
+func (c *Config) staging_dir_of(t time.Time, ext string) string {
 	// TODO: apply timezone shift
-	return staging_root + t.Format("/2006-01-02") + dir_of_ext[ext]
+	return c.staging_root + t.Format("/2006-01-02") + c.dir_of_ext[ext]
 }
 
 func group_by_staging(items []ItemInfo) map[string][]ItemInfo {
